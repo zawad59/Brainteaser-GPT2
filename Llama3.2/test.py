1e67f67a2bd50b44b022380fc83d13dfec6b9569
zawad59 <tawsikzawad@gmail.com>
2024-12-05 13:30:04 -0500

    Update test.py

--- Llama3.2/test.py
+++ Llama3.2/test.py
@@ -12,7 +12,7 @@ MAX_NEW_TOKENS = 50
 RESULTS_DIR = "llama-brainteasers-results/test"
 CHECKPOINTS_DIR = "/home/jawadkk/Brainteaser-GPT2/Llama3.2/"
 LEARNING_RATES = [0.01]
-WEIGHT_DECAYS = [0.0.0001]
+WEIGHT_DECAYS = [0.0001]
 
 # Ensure results directory exists
 os.makedirs(RESULTS_DIR, exist_ok=True)
